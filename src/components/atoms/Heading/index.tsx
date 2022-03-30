3e5c88946a6b352ab8d9e8f388add6fe0df76636
Dan To Hoa <dan.to@3forcom.com.vn>
2022-03-30 17:58:04 +0700

    :sparkles: implement title section

--- src/components/atoms/Heading/index.tsx
+++ src/components/atoms/Heading/index.tsx
@@ -6,7 +6,7 @@ type Size = 'lg';
 
 export type TextStyle = (GeneralTextStyle | Size)[];
 
-interface HeadingProps extends React.HtmlHTMLAttributes<HTMLHeadingElement> {
+export interface HeadingProps extends React.HtmlHTMLAttributes<HTMLHeadingElement> {
   type?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   content?: string;
   modifiers?: TextStyle;
@@ -40,9 +40,6 @@ const Heading: React.FC<HeadingProps> = ({
 };
 
 Heading.defaultProps = {
-  type: 'h2',
-  modifiers: undefined,
-  content: undefined,
 };
 
 export default React.memo(Heading);
---
+++ src/components/molecules/Title/index.tsx
@@ -0,0 +1,49 @@
+import React, { useLayoutEffect, useRef } from 'react';
+import { useSpring, animated } from 'react-spring';
+
+import Heading, { HeadingProps } from 'components/atoms/Heading';
+import useScrollAnimate from 'hooks/useScrollAnimation';
+
+interface TitleProps extends HeadingProps {
+}
+
+const Title: React.FC<TitleProps> = (rest) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const animate = useScrollAnimate(ref);
+  const [props, api] = useSpring(() => ({
+    from: {
+      opacity: 0,
+    },
+  }));
+
+  useLayoutEffect(() => {
+    if (animate) {
+      api.start({
+        from: {
+          opacity: 0,
+        },
+        to: {
+          opacity: 1,
+        },
+        config: {
+          duration: 1000,
+        },
+      });
+    }
+  }, [animate, api]);
+
+  return (
+    <div className="m-title">
+      <animated.div className="m-title_trail" style={props}>
+        <div ref={ref}>
+          <Heading {...rest} />
+        </div>
+      </animated.div>
+    </div>
+  );
+};
+
+Title.defaultProps = {
+};
+
+export default Title;
