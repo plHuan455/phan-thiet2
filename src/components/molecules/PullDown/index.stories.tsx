9d4fcc771af886700ce75d9fc4a1c60cbb052634
Nguyễn Việt Hoàng <hoang.nguyenviet@3forcom.com.vn>
2022-03-30 10:42:16 +0700

    :sparkles: feat(feature/NP-M-Pulldown): Implement M Pulldown

---
+++ src/components/molecules/PullDown/index.stories.tsx
@@ -0,0 +1,77 @@
+import { Story, Meta } from '@storybook/react';
+import React, { useState } from 'react';
+
+import Pulldown, { OptionType } from '.';
+
+export default {
+  title: 'Components/molecules/Pulldown',
+  component: Pulldown,
+  argTypes: {
+    label: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: '',
+    },
+    isClear: {
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: false,
+    },
+    error: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: '',
+    },
+    variant: {
+      control: {
+        type: 'select',
+        options: ['normal', 'highLight'],
+      },
+      defaultValue: 'normal',
+    },
+  },
+} as Meta;
+
+export const normal: Story = ({
+  label,
+  error,
+  isClear,
+  variant,
+}) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [value, setValue] = useState<OptionType>();
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+      }}
+    >
+      <Pulldown
+        variant={variant}
+        label={label}
+        error={error}
+        value={value}
+        placeholder="Ngày đăng mới nhất"
+        options={[
+          { id: '1', value: '1', label: '1' },
+          { id: '2', value: '2', label: '2' },
+          { id: '3', value: '3', label: '3' },
+          { id: '4', value: '4', label: '4' },
+          { id: '5', value: '5', label: '5' },
+          { id: '6', value: '6', label: '6' },
+          { id: '7', value: '7', label: '7' },
+          { id: '8', value: '8', label: '8' },
+          { id: '9', value: '9', label: '9' },
+
+        ]}
+        handleSelect={(data) => setValue(data)}
+        handleClear={() => setValue(undefined)}
+        isClear={isClear}
+      />
+    </div>
+  );
+};
---
+++ src/components/molecules/PullDown/index.tsx
@@ -0,0 +1,143 @@
+/* eslint-disable react/require-default-props */
+import React, { useEffect, useRef, useState } from 'react';
+
+import Icon from 'components/atoms/Icon';
+import useClickOutside from 'hooks/useClickOutside';
+import mapModifiers from 'utils/functions';
+
+export interface OptionType {
+  id?: string;
+  label: string;
+  value: string | number;
+}
+
+export interface PulldownProps {
+  id?: string;
+  label?: string;
+  required?: boolean;
+  placeholder?: string;
+  value?: OptionType;
+  options: OptionType[];
+  error?: string;
+  disabled?: boolean;
+  isClear?: boolean;
+  variant?: 'normal' | 'highLight';
+  size?: 'md' | 'lg';
+  handleSelect?: (option?: OptionType) => void;
+  handleClear?: () => void;
+}
+
+const renderLabel = (
+  info: Pick<PulldownProps, 'label' | 'id' | 'required'>,
+) => {
+  const { label, id, required } = info;
+
+  if (!label) return null;
+
+  return (
+    <label className="m-pulldown_label" htmlFor={id}>
+      {label}
+      {required && <span>*</span>}
+    </label>
+  );
+};
+
+const Pulldown: React.FC<PulldownProps> = ({
+  id,
+  label,
+  required,
+  placeholder,
+  value,
+  options,
+  error,
+  disabled,
+  isClear,
+  variant = 'normal',
+  handleSelect,
+  handleClear,
+}) => {
+  const pulldownRef = useRef<HTMLDivElement>(null);
+  const [optionData, setOptionData] = useState<OptionType[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggling = () => {
+    if (!disabled) {
+      setIsOpen(!isOpen);
+    }
+  };
+
+  useClickOutside(pulldownRef, () => {
+    if (isOpen) setIsOpen(false);
+  });
+
+  useEffect(() => {
+    setOptionData(options);
+  }, [options]);
+
+  return (
+    <div
+      className={`${mapModifiers('m-pulldown', error && 'error', variant)}`}
+      ref={pulldownRef}
+    >
+      {renderLabel({
+        label,
+        id,
+        required,
+      })}
+      <div className="m-pulldown_header" onClick={toggling}>
+        <div
+          className={`m-pulldown_header_content${
+            value ? '' : ' m-pulldown_placeholder'
+          }`}
+        >
+          <span>{value ? value.label : placeholder}</span>
+        </div>
+        <div className="m-pulldown_fn">
+          {isClear && value && (
+          <div className="m-pulldown_clear" onClick={handleClear}>
+            <Icon
+              iconName={variant === 'highLight' ? 'closeOrange' : 'closeBlack'}
+              size="16"
+            />
+          </div>
+          )}
+          <span className={isOpen ? 'm-pulldown_arrow opened' : 'm-pulldown_arrow'} />
+        </div>
+      </div>
+      {isOpen && (
+        <div className="m-pulldown_wrapper">
+          <ul className="m-pulldown_list">
+            {optionData.length ? (
+              optionData.map((option, index) => (
+                <li
+                  key={`option-${index.toString()}`}
+                  className={mapModifiers(
+                    'm-pulldown_item',
+                    value?.label === option.label && 'active',
+                  )}
+                  onClick={() => {
+                    if (handleSelect) {
+                      handleSelect(option);
+                      setIsOpen(false);
+                    }
+                  }}
+                >
+                  <span>{option.label}</span>
+                </li>
+              ))
+            ) : (
+              // TODO: Add translation later
+              <li className="m-pulldown_item none">No Option</li>
+            )}
+          </ul>
+        </div>
+      )}
+      {error && <span className="m-pulldown_error">{error}</span>}
+    </div>
+  );
+};
+
+Pulldown.defaultProps = {
+};
+
+export default Pulldown;
