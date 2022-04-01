cf52571caf69f9fe74506a819f3a44a2a2bb32b4
tiennguyenminh <tien.nguyen@3forcom.com>
2022-04-01 10:38:08 +0700

    update

--- src/assets/dataDummy/banner.ts
+++ src/assets/dataDummy/banner.ts
@@ -1,3 +1,38 @@
-const bannerData:string[] = [];
+import srcBanner from 'assets/images/bannerHome/banner_home.jpg';
+import imgCard from 'assets/images/bannerHome/card_1.png';
+import fly from 'assets/images/bannerHome/fly.png';
+import imgSub from 'assets/images/bannerHome/sub_1.png';
 
-export default bannerData;
+const list = [
+  {
+    srcImgMain: imgCard,
+    srcImgSub: imgSub,
+    title: 'DU LỊCH VÀ <br/> NGHỈ DƯỠNG',
+  },
+  {
+    srcImgMain: imgCard,
+    srcImgSub: imgSub,
+    title: 'VUI CHƠI <br/> & GIẢI TRÍ',
+  },
+  {
+    srcImgMain: imgCard,
+    srcImgSub: imgSub,
+    title: 'DU LỊCH <br/> MICE',
+    srcLayer: fly,
+  },
+  {
+    srcImgMain: imgCard,
+    srcImgSub: imgSub,
+    title: 'WELLNESS <br/> CHĂM SÓC <br/>SỨC KHỎE',
+  },
+];
+
+const description = 'Novaworld Phan Thiet - Siêu đô thị biển, du lịch nghỉ dưỡng, giải trí & chăm sóc sức khỏe. <br/> Kiến tạo điểm đến xứng tầm vị thế.';
+
+export default {
+  list,
+  description,
+  banner: {
+    src: srcBanner,
+  },
+};
