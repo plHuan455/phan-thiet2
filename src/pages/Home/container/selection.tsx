a4a1e9968de44b6b91af5c82083c73dd630c1d55
Dan To Hoa <dan.to@3forcom.com.vn>
2022-04-07 13:47:28 +0700

    :sparkles: fix defect

--- src/pages/Home/container/selection.tsx
+++ src/pages/Home/container/selection.tsx
@@ -4,7 +4,7 @@ import selectionImage from 'assets/images/selection/image.png';
 import SelectionTemplate from 'components/templates/Selection';
 
 const Selection: React.FC = () => (
-  <section className="u-mt-xl-206 u-mt-lg-160 u-mt-sm-80 u-mt-36">
+  <section className="u-mt-xl-206 u-mt-lg-160 u-mt-0">
     <SelectionTemplate
       title="LỰA CHỌN HOÀN HẢO <br />CHO ĐẦU TƯ, DU LỊCH <br />VÀ NGHỈ DƯỠNG"
       image={selectionImage}
