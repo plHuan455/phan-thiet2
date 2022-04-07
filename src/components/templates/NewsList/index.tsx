80d907f6a278bfdcc33bc566e321adffca587d43
Dan To Hoa <dan.to@3forcom.com.vn>
2022-04-07 15:12:53 +0700

    :sparkles: fix defect

--- src/components/templates/NewsList/index.tsx
+++ src/components/templates/NewsList/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 import Container from 'common/Container';
 import Button from 'components/atoms/Button';
-import Link from 'components/atoms/Link';
 import Text from 'components/atoms/Text';
 import Title from 'components/molecules/Title';
 import Card from 'components/organisms/Card';
@@ -24,36 +24,35 @@ const NewsList: React.FC<NewsListProps> = ({
       <div className="t-newsList_title">
         <Title type="h4" modifiers={['s015', 'gradientGreen', '700']} content={title} />
       </div>
-      {
-        listNews && listNews.length > 0 ? (
-          <div className="t-newsList_content u-mt-md-32 u-mt-16">
-            <div className="t-newsList_list u-mt-negative-8 u-mb-negative-8 u-mt-negative-md-16 u-mb-negative-md-16">
-              {
-                listNews.map((item, index) => (
-                  <div key={`t-newsList-${index.toString()}`} className="t-newsList_list-item u-pt-8 u-pb-8 u-pt-md-16 u-pb-md-16">
-                    <Card.News
-                      {...item}
-                    />
-                  </div>
-                ))
-              }
+      {listNews?.length ? (
+        <>
+          <Row className="u-ml-negative-16 u-mr-negative-16">
+            {listNews.map((item, index) => (
+              <Col
+                xs={12}
+                sm={6}
+                lg={12}
+                key={`t-newsList-${index.toString()}`}
+                className={`${index === 0 ? 'u-mt-lg-45' : 'u-mt-lg-59'} u-mt-md-32 u-mt-24`}
+              >
+                <Card.News
+                  {...item}
+                />
+              </Col>
+            ))}
+          </Row>
+          {button && (
+            <div className="t-newsList_button d-flex justify-content-center u-mt-24 u-mt-md-45">
+              <Button variant="primary-green">{button?.text}</Button>
             </div>
-            {
-              button && (
-                <div className="t-newsList_button u-mt-16 u-mt-md-32">
-                  <Link href={button?.url} target={button?.target}>
-                    <Button variant="primary-green">{button?.text}</Button>
-                  </Link>
-                </div>
-              )
-            }
-          </div>
-        ) : (
-          <div className="u-mt-16 u-mt-md-32">
-            <Text modifiers={['gradientGreen', '24x36', 'center']} content="Không có dữ liệu" />
-          </div>
-        )
-      }
+          )}
+        </>
+      ) : (
+        <div className="u-mt-16 u-mt-md-32">
+          {/* TODO: Translate later */}
+          <Text modifiers={['gradientGreen', '24x36', 'center']} content="Không có dữ liệu" />
+        </div>
+      )}
     </Container>
   </div>
 );
