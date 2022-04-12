621b936751213fc37906113b699f948b38074a2a
Dan To Hoa <dan.to@3forcom.com.vn>
2022-04-12 11:00:41 +0700

    :sparkles: implement page division detail

---
+++ src/pages/DivisionDetail/container/banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import banner from 'assets/images/banner.png';
+import DivisionBanner from 'components/templates/DivisionBanner';
+
+const Banner: React.FC = () => (
+  <DivisionBanner thumbnail={banner} title="NHỊP ĐẬP THẾ GIỚI MỚI" />
+);
+
+export default Banner;
