6383eb9e483e16e0d59ca845327156e19a24b79d
Dan To Hoa <dan.to@3forcom.com.vn>
2022-04-14 10:50:01 +0700

    :hammer: fix syntax

--- src/pages/Contact/container/banner.tsx
+++ src/pages/Contact/container/banner.tsx
@@ -4,14 +4,18 @@ import BannerTemplate from 'components/templates/Banner';
 import { baseURL, getBannerData } from 'utils/functions';
 
 const Banner: React.FC<SectionBanners> = ({ banners }) => {
-  const bannerData = useMemo(() => ({
-    image: banners.map((item) => ({
-      src: baseURL(item.data.imageDesktop),
-      srcTablet: baseURL(item.data.imageTablet),
-      srcMobile: baseURL(item.data.imageMobile),
-    }))[0],
-    title: getBannerData(banners[0].type, banners)?.title,
-  }), [banners]);
+  const bannerData = useMemo(() => {
+    const banner = getBannerData('basic', banners);
+    return ({
+      title: banner?.title,
+      image: {
+        src: baseURL(banner?.imageDesktop),
+        srcTablet: baseURL(banner?.imageTablet),
+        srcMobile: baseURL(banner?.imageMobile),
+        alt: banner?.title,
+      },
+    });
+  }, [banners]);
 
   return (
     <BannerTemplate
