eb159ed31854631ae9d91eae8201f941ecf38f63
tiennguyenminh <tien.nguyen@3forcom.com>
2022-04-15 13:35:07 +0700

    :hammer:feat(NP-86-division-location): update line division

--- src/components/templates/DivisionLocation/index.stories.tsx
+++ src/components/templates/DivisionLocation/index.stories.tsx
@@ -10,10 +10,11 @@ export default {
 } as Meta;
 
 export const normal: Story = () => (
-  <DivisionLocation
-    color="#1B468A"
-    title="VỊ TRÍ DỰ ÁN"
-    titleBox="THE KINGDOM"
-    contentBox="Nơi tinh hoa năm châu hội tự với những tiện ích hàng đầu, nơi các dãy biệt thự ôm trọn cả “thế giới”, The Kingdom là điểm giao huyết mạch, là trái tim của NovaWorld Phan Thiet"
-  />
+  <div style={{ color: '#F0B81C', paddingTop: '100vh', paddingBottom: '100vh' }}>
+    <DivisionLocation
+      title="VỊ TRÍ DỰ ÁN"
+      titleBox="THE KINGDOM"
+      contentBox="Nơi tinh hoa năm châu hội tự với những tiện ích hàng đầu, nơi các dãy biệt thự ôm trọn cả “thế giới”, The Kingdom là điểm giao huyết mạch, là trái tim của NovaWorld Phan Thiet"
+    />
+  </div>
 );
--- src/components/templates/DivisionLocation/index.tsx
+++ src/components/templates/DivisionLocation/index.tsx
@@ -1,79 +1,72 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import BoxDivision, { BoxDivisionProps } from './boxDivision';
-import Boutique from './components/boutique';
-import FestivalStreet from './components/festivalStreet';
-import FestivalTown from './components/festivalTown';
-import FloridaFirst from './components/floridaFirst';
-import FloridaSecond from './components/floridaSecond';
-import Kingdom from './components/kingdom';
-import OceanResidence from './components/oceanResidence';
-import PgaGolf from './components/pgaGolf';
-import Santa from './components/santa';
-import Waikiki from './components/waikiki';
+import mapData from './constants';
 
 import bg from 'assets/images/divisionLocation/bg.jpg';
 import Container from 'common/Container';
 import Heading from 'components/atoms/Heading';
+import Image from 'components/atoms/Image';
+import useScrollAnimate from 'hooks/useScrollAnimation';
 
 interface DivisionLocationProps extends BoxDivisionProps {
   title?: string;
-  color?: string;
 }
 
 const DivisionLocation: React.FC<DivisionLocationProps> = ({
   titleBox,
   contentBox,
   title,
-  color,
-}) => (
-  <div className="t-divisionLocation">
-    <Container>
-      <div style={{ color }}>
+}) => {
+  const ref = useRef<HTMLDivElement|null>(null);
+  const isScroll = useScrollAnimate(ref, 3);
+
+  return (
+    <div
+      ref={ref}
+      className="t-divisionLocation"
+    >
+      <Container>
         <Heading type="h2" modifiers={['inherit', 's015', 'uppercase']} content={title} />
-      </div>
-    </Container>
-    <div className="t-divisionLocation_wrapContent u-mt-30 u-mt-md-56">
-      <div className="t-divisionLocation_map">
-        <img className="t-divisionLocation_bg" src={bg} loading="lazy" alt="bg" />
-        <FloridaSecond.ImageShine />
-        <FestivalStreet.ImageShine />
-        <FestivalTown.ImageShine />
-        <PgaGolf.ImageShine />
-        <FloridaFirst.ImageShine />
-        <Santa.ImageShine />
-        <Waikiki.ImageShine />
-        <Boutique.ImageShine />
-        <OceanResidence.ImageShine />
-        <Kingdom.ImageShine />
-        <div className="t-divisionLocation_svg">
-          <svg width="100%" height="100%" viewBox="0 0 1366 780" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-            <FloridaSecond.Line />
-            <FestivalStreet.Line />
-            <FestivalTown.Line />
-            <PgaGolf.Line />
-            <FloridaFirst.Line />
-            <Santa.Line />
-            <Waikiki.Line />
-            <Boutique.Line />
-            <OceanResidence.Line />
-            <Kingdom.Line />
-          </svg>
+      </Container>
+      <div className="t-divisionLocation_wrapContent u-mt-30 u-mt-md-56">
+        <div className="t-divisionLocation_map">
+          <img className="t-divisionLocation_bg" src={bg} loading="lazy" alt="bg" />
+          {mapData.map((item, index) => (
+            <div
+              key={`image-${index.toString()}`}
+              className={`division-image ${item.className} ${isScroll ? 'active' : ''}`}
+            >
+              <Image src={item.img} alt={item.className} />
+            </div>
+          ))}
+          <div className="t-divisionLocation_svg">
+            <svg width="100%" height="100%" viewBox="0 0 1366 780" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+              {mapData.map((item, index) => (
+                <path
+                  key={`line-${index.toString()}`}
+                  className={`division-line ${isScroll ? 'active' : ''}`}
+                  d={item.line}
+                  stroke="#FFFEF8"
+                  strokeWidth="6"
+                />
+              ))}
+            </svg>
+          </div>
+        </div>
+        <div className={`t-divisionLocation_box ${isScroll ? 'active' : ''}`}>
+          <BoxDivision
+            titleBox={titleBox}
+            contentBox={contentBox}
+          />
         </div>
       </div>
-      <div className="t-divisionLocation_box active" style={{ color }}>
-        <BoxDivision
-          titleBox={titleBox}
-          contentBox={contentBox}
-        />
-      </div>
     </div>
-  </div>
-);
+  );
+};
 
 DivisionLocation.defaultProps = {
   title: undefined,
-  color: undefined,
 };
 
 export default DivisionLocation;
