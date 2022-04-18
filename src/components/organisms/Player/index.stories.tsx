02f15947db24355883b4f92c0a4eef85979c6098
Hoang Cong Phan <hoang.phancong@3forcom.com.vn>
2022-04-18 10:06:04 +0700

    ✨ Fix: update icon player

--- src/components/organisms/Player/index.stories.tsx
+++ src/components/organisms/Player/index.stories.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { Story, Meta } from '@storybook/react';
-import React, { useRef, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 
 import Player from '.';
 
@@ -9,12 +9,20 @@ export default {
   component: Player,
   argTypes: {},
 } as Meta;
+export interface MyCustomCSS extends React.CSSProperties {
+  '--theme': string;
+}
 
 export const normal: Story = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [isPlay, setPlay] = useState(false);
+
+  const styles = useMemo((): MyCustomCSS => ({
+    '--theme': 'linear-gradient(180deg, rgb(0, 168, 168) 0%, rgb(2, 116, 127) 100%)',
+  }), []);
+
   return (
-    <div style={{ padding: 20, maxWidth: 974 }}>
+    <div style={{ padding: 20, maxWidth: 974, ...styles }}>
       <Player
         ref={videoRef}
         src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
