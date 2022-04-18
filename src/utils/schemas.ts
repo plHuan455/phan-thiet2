6334b1640660c76bf83fab01dc6cf70331447a78
Dan To Hoa <dan.to@3forcom.com.vn>
2022-04-18 10:40:18 +0700

    :hammer: fix regex phone number

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,14 +1,14 @@
 import * as yup from 'yup';
 
-export const phoneRegExp = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+export const phoneRegExp = /(84|0[3|5|7|8|9])+([0-9]{8})\b/;
 
 export const schemasConsultancyForm = yup.object({
   phone: yup
     .string()
-    .required('Yêu cầu nhập số điện thoại')
-    .matches(phoneRegExp, 'Không hợp lệ'),
+    .required('Vui lòng nhập số điện thoại !!')
+    .matches(phoneRegExp, 'Số điện thoại không hợp lệ !!'),
   email: yup
     .string()
-    .required('Yêu cầu nhập email')
-    .email('Không hợp lệ'),
+    .required('Vui lòng nhập email !!')
+    .email('Email Không hợp lệ !!'),
 });
