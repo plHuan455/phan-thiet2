05a8d5341060ef8474ea88cfa96bfe0b47777955
Nguyễn Việt Hoàng <hoang.nguyenviet@3forcom.com.vn>
2022-04-20 14:29:40 +0700

    :hammer: hotfix(Devisioncard): Update Division Card

--- src/components/organisms/Card/Division/index.tsx
+++ src/components/organisms/Card/Division/index.tsx
@@ -22,21 +22,23 @@ const CardDivision: React.FC<CardDivisionProps> = ({
 }) => (
   <div className="m-subDivisionCard">
     <Link href={href} target={target} className="animate animate-arrowSlide">
-      <div className="m-subDivisionCard_image">
-        <Image src={imgSrc} ratio="354x221" alt={title} />
-      </div>
-      <div className="m-subDivisionCard_content">
-        <div className="m-subDivisionCard_content-title">
-          <Text content={title} modifiers={['20x32', '700', 'raisinBlack', 's015']} />
+      <div className="m-subDivisionCard_wrap">
+        <div className="m-subDivisionCard_image">
+          <Image src={imgSrc} ratio="354x221" alt={title} />
         </div>
-        <div className="m-subDivisionCard_desc u-mt-8">
-          <Text content={description} modifiers={['12x20', '400', 'davyGrey']} />
-        </div>
-        <div className="d-flex align-items-center u-mt-12">
-          {/* // TODO: Translation */}
-          <Text modifiers={['14x20', '400', 'copper']} content="Xem thêm" />
-          <div className="u-ml-8">
-            <Icon iconName="arrowRightCopper" size="16" />
+        <div className="m-subDivisionCard_content">
+          <div className="m-subDivisionCard_content-title">
+            <Text content={title} modifiers={['20x32', '700', 'raisinBlack', 's015']} />
+          </div>
+          <div className="m-subDivisionCard_desc u-mt-8">
+            <Text content={description} modifiers={['12x20', '400', 'davyGrey']} />
+          </div>
+          <div className="d-flex align-items-center u-mt-12">
+            {/* // TODO: Translation */}
+            <Text modifiers={['14x20', '400', 'copper']} content="Xem thêm" />
+            <div className="u-ml-8">
+              <Icon iconName="arrowRightCopper" size="16" />
+            </div>
           </div>
         </div>
       </div>
