1782bb3aca435137998293ffc1b76ed3d58332ef
tiennguyenminh <tien.nguyen@3forcom.com>
2022-04-22 17:16:25 +0700

    :hammer:update

--- src/components/templates/DivisionJourneys/index.stories.tsx
+++ src/components/templates/DivisionJourneys/index.stories.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { Story, Meta } from '@storybook/react';
-import React, { useState } from 'react';
+import React from 'react';
 
 import DivisionJourneys from '.';
 
@@ -9,28 +8,26 @@ import divisionJourneysData from 'assets/dataDummy/divisionJourneys';
 export default {
   title: 'Components/templates/DivisionJourneys',
   component: DivisionJourneys,
-  argTypes: {
-    loading: {
-      type: 'boolean',
-    },
-  },
+  argTypes: {},
 } as Meta;
 
-export const normal: Story = ({
-  loading,
-}) => {
-  const [slugActive, setSlugActive] = useState<string|undefined>('nova-world-center');
-
-  return (
-    <div style={{ minHeight: '1000px' }}>
-      <DivisionJourneys
-        {...divisionJourneysData}
-        loading={loading}
-        textNotFound="Không tìm thấy dữ liệu"
-        slugActive={slugActive}
-        handleClick={(slug) => setSlugActive(slug)}
-        title="HÀNH TRÌNH TRẢI NGHIỆM SIÊU THÀNH PHỐ BIỂN"
-      />
-    </div>
-  );
-};
+export const normal: Story = () => (
+  <div style={{ minHeight: '1000px' }}>
+    <DivisionJourneys
+      {...divisionJourneysData}
+      textNotFound="Không tìm thấy dữ liệu"
+      title="HÀNH TRÌNH TRẢI NGHIỆM SIÊU THÀNH PHỐ BIỂN"
+      idActive={1}
+      tabs={[
+        {
+          name: 'Công viên biển Miami Bikini Beach',
+          id: 1,
+        },
+        {
+          name: 'Công viên giải trí NovaDreams Adventure Park',
+          id: 2,
+        },
+      ]}
+    />
+  </div>
+);
