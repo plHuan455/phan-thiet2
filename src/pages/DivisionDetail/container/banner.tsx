555ba3317521feb262033e7d8b47df776ea4cce9
Nguyễn Việt Hoàng <hoang.nguyenviet@3forcom.com.vn>
2022-04-26 17:29:09 +0700

    :bug: hotfix(NP-DivisionLocation): Hotfix Responsive

--- src/pages/DivisionDetail/container/banner.tsx
+++ src/pages/DivisionDetail/container/banner.tsx
@@ -7,12 +7,12 @@ const Banner: React.FC<SectionBanners> = ({ banners }) => {
   const banner = useMemo(() => [
     {
       data: {
-        link: banners[0].data?.link,
-        title: banners[0].data?.title,
-        subTitle: banners[0].data?.subTitle,
-        imageMobile: baseURL(banners[0].data?.imageMobile),
-        imageTablet: baseURL(banners[0].data?.imageTablet),
-        imageDesktop: baseURL(banners[0].data?.imageDesktop),
+        link: banners[0]?.data?.link,
+        title: banners[0]?.data?.title,
+        subTitle: banners[0]?.data?.subTitle,
+        imageMobile: baseURL(banners[0]?.data?.imageMobile),
+        imageTablet: baseURL(banners[0]?.data?.imageTablet),
+        imageDesktop: baseURL(banners[0]?.data?.imageDesktop),
       },
     },
   ], [banners]);
