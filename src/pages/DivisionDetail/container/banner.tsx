9ae8c4049955038fbb7a0da76646ecdf13b1e52e
Dan To Hoa <dan.to@3forcom.com.vn>
2022-04-26 17:05:42 +0700

    :hammer: update syntax

--- src/pages/DivisionDetail/container/banner.tsx
+++ src/pages/DivisionDetail/container/banner.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import DivisionBanner, { DivisionBannerProps } from 'components/templates/DivisionBanner';
+import DivisionBanner from 'components/templates/DivisionBanner';
+import { baseURL } from 'utils/functions';
 
-interface BannerProps extends DivisionBannerProps {}
-
-const Banner: React.FC<BannerProps> = (props) => (
-  <DivisionBanner {...props} />
-);
+const Banner: React.FC<SectionBanners> = ({ banners }) => {
+  const banner = useMemo(() => [
+    {
+      data: {
+        link: banners[0].data?.link,
+        title: banners[0].data?.title,
+        subTitle: banners[0].data?.subTitle,
+        imageMobile: baseURL(banners[0].data?.imageMobile),
+        imageTablet: baseURL(banners[0].data?.imageTablet),
+        imageDesktop: baseURL(banners[0].data?.imageDesktop),
+      },
+    },
+  ], [banners]);
+  return (
+    <DivisionBanner banner={banner} />
+  );
+};
 
 export default Banner;
