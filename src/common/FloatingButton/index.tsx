f5191a939d088feb6b379b0bc6802f64b36f9667
Dan To Hoa <dan.to@3forcom.com.vn>
2022-05-06 14:28:59 +0700

    :hammer: scroll eneble facebooksdk

--- src/common/FloatingButton/index.tsx
+++ src/common/FloatingButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import Icon from 'components/atoms/Icon';
 import useWindowScroll from 'hooks/useWindowScroll';
@@ -11,6 +11,8 @@ const FloatingButton: React.FC = () => {
   const dataSystem = useAppSelector((state) => state.system.data);
   const [isShow, setIsShow] = useState(false);
   const { language } = i18n;
+  const isFirst = useRef(true);
+
   useWindowScroll(() => {
     if (window.pageYOffset > 200) {
       setIsShow(true);
@@ -26,12 +28,13 @@ const FloatingButton: React.FC = () => {
     });
   };
 
-  useEffect(() => {
-    if (dataSystem?.messengerId) {
+  useWindowScroll(() => {
+    if (dataSystem?.messengerId && isFirst.current && window.pageYOffset > 100) {
       initFacebookSdk(language, dataSystem.messengerId);
+      isFirst.current = false;
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dataSystem]);
+  });
 
   return (
     <div className="floatingButton">
